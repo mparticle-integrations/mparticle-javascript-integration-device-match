b85fdfde14b805e87bed703deb4f2c2681faf898
Robert Ing <robertming@gmail.com>
2020-07-27 10:06:26 -0400

    Feat - allow testing via CLI (#7)

--- test/tests.js
+++ test/tests.js
@@ -1,31 +1,35 @@
-describe('Device Match Forwarder', function () {
-    var ReportingService = function () {
-        var self = this;
+describe('Device Match Forwarder', function() {
+    var ReportingService = function() {
+            var self = this;
 
-        this.id = null;
-        this.event = null;
+            this.id = null;
+            this.event = null;
 
-        this.cb = function (forwarder, event) {
-            self.id = forwarder.id;
-            self.event = event;
-        };
+            this.cb = function(forwarder, event) {
+                self.id = forwarder.id;
+                self.event = event;
+            };
 
-        this.reset = function () {
-            self.id = null;
-            self.event = null;
-        };
-    },
-    reportService = new ReportingService();
+            this.reset = function() {
+                self.id = null;
+                self.event = null;
+            };
+        },
+        reportService = new ReportingService();
 
-    beforeEach(function () {
+    beforeEach(function() {
         reportService.reset();
-        mParticle.forwarder.init({
-            partnerId: 123456,
-        }, reportService.cb, false);
+        mParticle.forwarder.init(
+            {
+                partnerId: 123456,
+            },
+            reportService.cb,
+            false
+        );
     });
 
-    describe('Init the Device Match forwarder', function () {
-        it('should init', function (done) {
+    describe('Init the Device Match forwarder', function() {
+        it('should init', function(done) {
             var noscript = document.getElementsByTagName('noscript')[0];
             noscript.should.not.be.undefined();
 
